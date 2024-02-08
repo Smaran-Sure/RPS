877e213465a15601d33f3e63f179097477a95ebe
Smaran Sure <smaran.sure@gmail.com>
2024-02-07 22:31:31 -0800

    Added RPS UI

--- javascript.js
+++ javascript.js
@@ -1,9 +1,36 @@
 let user = 0;
 let bot = 0;
+const display1 = document.querySelector(".t1");
+const display2 = document.querySelector(".t2");
+const display3 = document.querySelector(".t3");
+const display4 = document.querySelector(".t4");
+const display5 = document.querySelector(".t5");
+ending = document.querySelector("#end");
+endP = document.querySelector(".p-end");
 function game() {
-    
+    let playButtons = document.querySelectorAll(".play");
+    playButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let player = button.id;
+        if (user <5 && bot <5) {
+            playRound(player);
+        }
+    });
+    let redoButton = document.querySelector("#restart");
+    redoButton.addEventListener("click", () => {
+        user = 0;
+        bot = 0;
+        display1.textContent = "Start playing by clicking Rock, Paper or Scissors";
+        display2.textContent = "Bot VS You";
+        display3.textContent = "Win/Lose";
+        display4.textContent = "Bot: 0 pts";
+        display5.textContent = "You: 0 pts";
+        endP.textContent = "";
+        ending.textContent = "Scores: null";
+
+    })
+})
     function playRound(playerSelection, computerSelection) {
-        playerSelection = prompt("Choose: Rock|Paper|Scissors", "").toLowerCase();
         getComputerChoice = (list) =>  {
             const randomIndex = Math.floor(Math.random() * list.length);
 
@@ -14,65 +41,68 @@ function game() {
         };
         const choices = ["rock","paper","scissors"];
         computerSelection = getComputerChoice(choices);
-        console.log(`Bot: ${computerSelection}`);
-        console.log(`You: ${playerSelection}`);
+        display1.textContent = (`Bot: ${computerSelection}`);
+        display2.textContent = (`You: ${playerSelection}`);
         if (playerSelection == computerSelection) {
-            console.log("Tie! No one gets a point.");
+            display3.textContent = "Tie! No one gets a point.";
         } 
         else if (playerSelection == "rock") {
             if (computerSelection == "scissors") {
-                console.log("You won | Rock beats Scissors");
+                display3.textContent = ("You won | Rock beats Scissors");
                 user = user+1;
-                console.log("You have "+user.toString()+" points");
-                console.log("The bot has "+bot.toString()+" points");
+                display4.textContent = ("You have "+user.toString()+" points");
+                display5.textContent = ("The bot has "+bot.toString()+" points");
             }
             else if (computerSelection == "paper") {
-                console.log("You lose | Paper beats Rock");
+                display3.textContent = ("You lose | Paper beats Rock");
                 bot = bot + 1;
-                console.log("You have "+user.toString()+" points");
-                console.log("The bot has "+bot.toString()+" points");
+                display4.textContent = ("You have "+user.toString()+" points");
+                display5.textContent = ("The bot has "+bot.toString()+" points");
             }}
         else if (playerSelection == "scissors") {
             if (computerSelection == "rock") {
-                console.log("You lose | Rock beats Scissors");
+                display3.textContent = ("You lose | Rock beats Scissors");
                 bot = bot + 1;
-                console.log("You have "+user.toString()+" points");
-                console.log("The bot has "+bot.toString()+" points");
+                display4.textContent = ("You have "+user.toString()+" points");
+                display5.textContent = ("The bot has "+bot.toString()+" points");
             }
             else if (computerSelection == "paper") {
-                console.log("You won | Scissors beats Paper");
+                display3.textContent = ("You won | Scissors beats Paper");
                 user = user+1;
-                console.log("You have "+user.toString()+" points");
-                console.log("The bot has "+bot.toString()+" points");
+                display4.textContent = ("You have "+user.toString()+" points");
+                display5.textContent = ("The bot has "+bot.toString()+" points");
             }}
         else if (playerSelection == "paper") {
             if (computerSelection == "scissors") {
-                console.log("You lose | Scissors beats Paper");
+                display3.textContent = ("You lose | Scissors beats Paper");
                 bot = bot + 1;
-                console.log("You have "+user.toString()+" points");
-                console.log("The bot has "+bot.toString()+" points");
+                display4.textContent = ("You have "+user.toString()+" points");
+                display5.textContent = ("The bot has "+bot.toString()+" points");
             }
             else if (computerSelection == "rock") {
-                console.log("You win | Rock beats Paper")
+                display3.textContent = ("You win | Rock beats Paper")
                 user = user+1;
-                console.log("You have "+user.toString()+" points");
-                console.log("The bot has "+bot.toString()+" points");
+                display4.textContent = ("You have "+user.toString()+" points");
+                display5.textContent = ("The bot has "+bot.toString()+" points");
             }}
-        console.log("----------------------------------------")
+        if (user == 5 || bot == 5) {
+            endP.textContent = "Game has ended - Final Scores ->";
+            if (user > bot) {
+                ending.textContent = ("You beat the bot by "+(user-bot).toString()+" points");
+            }
+            if (bot > user) {
+                ending.textContent = ("The bot beat you by "+(bot-user).toString()+" points");
+            }
+        }
+       
 }
-playRound();
-playRound();
-playRound();
-playRound();
-playRound();
 }
 game();
-console.log("FINAL SCOREBOARD");
-console.log("You: "+user.toString()+" points");
-console.log("Bot: "+bot.toString()+" points");
-if (user > bot) {
-    console.log("You beat the bot by "+(user-bot).toString()+" points");
-}
-else if (bot > user) {
-    console.log("You lost. The bot beat you by "+(bot-user).toString()+" points");
-}
+
+
+
+
+
+
+
+ 
